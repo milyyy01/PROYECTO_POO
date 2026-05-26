fe0825f61887e1fdfa41a677b599acb5d2a255f4
Ariela Sánchez <angelina.sanan@gmail.com>
2026-05-26 15:44:00 -0500

    Implement Sede class with attributes and methods

--- models/sede.py
+++ models/sede.py
@@ -1,2 +1,32 @@
 class Sede:
-    pass
+    def __init__(self, id_sede, nombre_sede, direccion, ciudad, capacidad_total):
+        self.id_sede = id_sede
+        self.nombre_sede = nombre_sede
+        self.direccion = direccion
+        self.ciudad = ciudad
+        self.capacidad_total = capacidad_total
+        self.paralelos = []
+        self.carreras = []
+        self.ofertas = []
+
+    def calcular_disponibilidad(self):
+        cupos_ocupados = sum(oferta.cupos_ocupados for oferta in self.ofertas)
+        return self.capacidad_total - cupos_ocupados
+
+    def listar_paralelos(self):
+        return self.paralelos
+
+    def listar_carreras(self):
+        return self.carreras
+
+    def obtener_ofertas(self):
+        return self.ofertas
+
+    def agregar_paralelo(self, paralelo):
+        self.paralelos.append(paralelo)
+
+    def agregar_carrera(self, carrera):
+        self.carreras.append(carrera)
+
+    def agregar_oferta(self, oferta):
+        self.ofertas.append(oferta)
